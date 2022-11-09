6e66db380de281052d2c233dae8913b3d56f183a
kunalrathodofficial <102890375+kunalrathodofficial@users.noreply.github.com>
2022-11-09 22:03:16 +0530

    Create pointer03.cpp

---
+++ pointer03.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void call_by_reference_pointer(int& n){
+	cout<<"address of n in call_by_reference_function:"<<&n<<endl;
+	n*=n;
+
+}
+int main(){
+	
+	int n=8;
+	cout<<"address of n in main() :"<<&n<<endl;
+	call_by_reference_pointer(n);
+    cout<<"ans :"<<n<<endl;
+    cout<<"value of n:"<<n<<endl;
+	return 0;
+	
+}
