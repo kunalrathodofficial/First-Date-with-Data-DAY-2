d55286618a0a7175a9a40c060e79d0e0beab64ea
kunalrathodofficial <102890375+kunalrathodofficial@users.noreply.github.com>
2022-11-09 22:04:02 +0530

    Create pointer04.cpp

---
+++ pointer04.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	
+	int ar[5]={10,20,30,40,50};
+	
+	int *p=ar;
+	cout<<*p<<endl;
+	cout<<ar<<endl;
+	cout<<&ar[1]<<endl;
+	return 0;
+	
+}
